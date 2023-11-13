2c20d21f61fda96fa5795793abaa536ad019067a
정병산불곰 <myvj123@naver.com>
2023-11-13 15:03:10 +0900

    rm cache

--- target/classes/static/js/memberSecret.js
+++
@@ -1,2 +0,0 @@
-let username = [${#authentication.principal.nickname}];
-let userRole = [${#authentication.principal.authorities[0].authority}];
--- target/classes/static/js/pageActions.js
+++
@@ -1,9 +0,0 @@
-//뒤로가기
-function goBack() {
-    history.back();
-}
-
-//새로고침
-function refreshPage() {
-    location.reload();
-}
